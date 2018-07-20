9d7148f30b3f09ac022cad5428512b548a86e899
Sai Tarun Reddy Pallati <saitarunreddyp@gmail.com>
2018-07-20 17:24:48 +0530

    sai Tarun

--- Sample.js
+++ Sample.js
@@ -3,6 +3,7 @@
 <script type="text/javascript" src="message.js"></script>  
 </head>  
 <body>  
+<H1> Hello Tarun how are you doing</H1>    
 <p>Welcome to JavaScript tarun</p>  
 <form>  
 <input type="button" value="click" onclick="msg()"/>  
